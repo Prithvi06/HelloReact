11613d0885b8d28d5c866f5f050f6db9ba7d3cee
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-31 20:07:39 +0530

    modify footer

--- app.js
+++ app.js
@@ -69,6 +69,18 @@ const Footer = () => {
                 <li>link3</li>
                 <li>link4</li>
             </ul>
+             <ul className="footer-items">
+                <li>link1</li>
+                <li>link2</li>
+                <li>link3</li>
+                <li>link4</li>
+            </ul>
+            <ul className="footer-items">
+                <li>link1</li>
+                <li>link2</li>
+                <li>link3</li>
+                <li>link4</li>
+            </ul>
         </div>
     )
 }
