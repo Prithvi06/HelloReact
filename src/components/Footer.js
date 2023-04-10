00a106970ac2be8d7dd3258e742051bd13300144
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-04-10 23:23:47 +0530

    add item

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
                 <li>link4</li>
             </ul>
             <ul className="footer-items">
-                <li>link1</li>
-                <li>link2</li>
-                <li>link3</li>
-                <li>link4</li>
+                <li>item1</li>
+                <li>item2</li>
+                <li>item3</li>
+                <li>item4</li>
             </ul>
              <ul className="footer-items">
                 <li>link1</li>
