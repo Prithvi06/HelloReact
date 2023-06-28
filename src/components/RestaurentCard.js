94229a3d4699cf851ef33b8e0df601542113e349
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-06-28 23:22:53 +0530

    add link to restaurent card

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -1,16 +1,28 @@
 import {CARDIMG_URL} from "../utils/constants";
 
-const RestaurentCard = ({resData}) => {
-    const {resName, cuisine, rating} = {...resData}
+const RestaurentCard = (props) => {
+    const { resData } = props
+
+    const {
+        cloudinaryImageId,
+        name,
+        cuisines,
+        costForTwo,
+        deliveryTime,
+        avgRating
+    } = resData?.data
+
     return (
         <div className="res-card">
             <img
                 className="res-logo"
                 alt="res-logo" 
-                src={CARDIMG_URL} />
-            <h3>{resName}</h3>
-            <h4>{cuisine}</h4>
-            <h4>{rating}</h4>
+                src={CARDIMG_URL+cloudinaryImageId} />
+            <h3>{name}</h3>
+            <h5>{cuisines.join(", ")}</h5>
+            <h5>{costForTwo / 100} For Two</h5>
+            <h5>{deliveryTime} minutes</h5>
+            <h5>{avgRating}</h5>
         </div>
     )
 }
