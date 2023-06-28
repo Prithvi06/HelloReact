61273cbac997df02ca8ab715af2c16191d750890
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-06-28 23:22:19 +0530

    add link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
-
+import { useState } from "react"
 import header_logo from "../../header_logo.png"
+import { Link } from "react-router-dom";
+
 const Header = () => {
+    const [btnName, setBtnName] = useState("Login");
     return (
         <div className="header">
             <div className="logo-container">
@@ -11,10 +14,11 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about"> About Us</Link></li>
+                    <li><Link to={"/contact"}>Contact Us</Link></li>
                     <li>Cart</li>
+                    <button className="login-btn" onClick={() => { btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")} }>{btnName}</button>
                 </ul>
             </div>
         </div>
