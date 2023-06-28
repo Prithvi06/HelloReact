c2669136d6bdaf15f1998f937634ee2c8d15d05f
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-06-28 23:21:15 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,57 @@ import ReactDOM from 'react-dom/client';
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./components/about";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurentMenu from "./components/RestaurentMenu";
+
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
             <Footer />
         </div>
     );
 };
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            },
+            {
+                path: "/restaurent/:resId",
+                element: <RestaurentMenu />
+            }
+        ],
+        errorElement: <Error />
+    },
+    // {
+    //     path: "/about",
+    //     element: <About />
+    // },
+    // {
+    //     path: "/contact",
+    //     element: <Contact />
+    // }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+// root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />)
