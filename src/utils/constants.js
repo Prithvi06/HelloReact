1289745214aed5b8c01036dddfebb0017846671a
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-06-29 21:52:42 +0530

    add urls in constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const CARDIMG_URL =
-    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/g5txnz35wlrgbskk3r8y"
+    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+
+export const CARD_MENU_URL = 
+"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId="
