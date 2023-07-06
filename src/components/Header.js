cde36c86916fe4aa53098d7feaa9431f9caa8e71
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-07-06 23:10:38 +0530

    use customhooks in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import header_logo from "../../header_logo.png"
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
     const [btnName, setBtnName] = useState("Login");
+    
+    const onlineStatus = useOnlineStatus();
     return (
         <div className="header">
             <div className="logo-container">
@@ -14,6 +17,7 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
+                    <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about"> About Us</Link></li>
                     <li><Link to={"/contact"}>Contact Us</Link></li>
