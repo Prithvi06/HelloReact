918d0ffbea11040bc05da579e78c8bbd18b5ae15
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-07-17 23:17:33 +0530

    add collapse

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,23 @@
+import { useState } from "react"
 import ItemList from "./ItemList"
 
 const Category = ({data}) => {
+    const [showsItem, setShowsItem] = useState(false)
+
+    const handleItem = () => {
+        if (showsItem) {
+            setShowsItem(false)
+        } else {
+            setShowsItem(true)
+        }
+    }
     return (
-        <div className="w-6/12 bg-gray-50 mx-auto shadow-lg p-4 text-left m-3">
-            <div className="flex justify-between">
+        <div className="w-6/12 bg-gray-50 mx-auto shadow-lg p-4 text-left m-4">
+            <div className="flex justify-between  cursor-pointer" onClick={handleItem}>
                 <span className="font-bold">{data.title} ({data.itemCards.length})</span>
                 <span>⬇️</span>
             </div>
-            <ItemList items={data.itemCards} />
+            {showsItem && <ItemList items={data.itemCards} />}
         </div>
     )
 }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,15 +1,22 @@
+import { CARDIMG_URL } from "../utils/constants"
+
 const ItemList = ({items}) => {
     console.log("IIII", items)
     return (
         <div>
             {
                 items.map((item) => (
-                    <div key={item.card.info.id} className="p-4 border-b-2">
-                        <div className="flex gap-3">
-                            <span>{item.card.info.name}</span>
-                            <span>Rs. {(item.card.info.price ? item.card.info.price : item.card.info.defaultPrice)/100}</span>
+                    <div className="flex justify-between border-b-2">
+                        <div key={item.card.info.id} className="py-2  w-9/12">
+                            <div className="flex gap-3">
+                                <span>{item.card.info.name}</span>
+                                <span>Rs. {(item.card.info.price ? item.card.info.price : item.card.info.defaultPrice)/100}</span>
+                            </div>
+                            <p className="text-sm">{item.card.info.description}</p>
                         </div>
-                        <p className="text-sm">{item.card.info.description}</p>
+                        <div className="w-3/12 p-4">
+                            <img src={CARDIMG_URL+item.card.info.imageId} alt="" />
+                        </div>
                     </div>
                 ))
             }
