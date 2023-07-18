f73dfb0684e87d4b30ccf54600421a12d4ead39c
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-07-18 21:58:36 +0530

    change state

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,12 @@
 import { CARDIMG_URL } from "../utils/constants"
 
 const ItemList = ({items}) => {
-    console.log("IIII", items)
     return (
         <div>
             {
                 items.map((item) => (
-                    <div className="flex justify-between border-b-2">
-                        <div key={item.card.info.id} className="py-2  w-9/12">
+                    <div key={item.card.info.id} className="flex justify-between border-b-2">
+                        <div className="py-2  w-9/12">
                             <div className="flex gap-3">
                                 <span>{item.card.info.name}</span>
                                 <span>Rs. {(item.card.info.price ? item.card.info.price : item.card.info.defaultPrice)/100}</span>
