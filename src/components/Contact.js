97f214ae029a6231d362dd659f6cdab39fc8c57d
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-08-27 20:08:15 +0530

    update contact us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 const Contact = () => {
     return (
         <div>
-            Contact us page
+            <h1>Contact us page</h1>
+            <form action="">
+                <input type="text" className="border border-black p-2 m-2" placeholder="Name" />
+                <input type="text" name="" className="border border-black p-2 m-2" placeholder="Message" />
+                <button className="border border-black p-2 m-2 bg-gray-100 rounded-lg">Submit</button>
+            </form> 
         </div>
     )
 }
